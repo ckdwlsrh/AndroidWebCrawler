cbdcf26aa49043c4b3655bd16d5d8e2f43fb7f26
ckdwlsrh <ckdwls6504@gmail.com>
2021-12-24 13:14:11 +0900

    layout setting & binding setting

--- app/src/main/java/com/example/androidwebcrawler/MainActivity.kt
+++ app/src/main/java/com/example/androidwebcrawler/MainActivity.kt
@@ -2,10 +2,35 @@ package com.example.androidwebcrawler
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import com.example.androidwebcrawler.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        //뷰 바인딩
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        //password창 입력
+        binding.userpassword.addTextChangedListener(object: TextWatcher {
+            override fun afterTextChanged(s: Editable?) {
+                TODO("Not yet implemented")
+            }
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+                TODO("Not yet implemented")
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                TODO("Not yet implemented")
+            }
+        })
+        //로그인 버튼 리스너
+        binding.signin.setOnClickListener {
+
+        }
     }
 }
