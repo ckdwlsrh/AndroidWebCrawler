e9c396846e7c18511da827446a5f446ea83b3bb9
ckdwlsrh <ckdwls6504@gmail.com>
2021-12-26 20:22:49 +0900

    board crawling optimization

--- app/src/main/java/com/example/androidwebcrawler/JsoupCrawler.kt
+++ app/src/main/java/com/example/androidwebcrawler/JsoupCrawler.kt
@@ -1,47 +1,70 @@
 package com.example.androidwebcrawler
 
-import android.util.Log
 import org.jsoup.Connection
 import org.jsoup.Jsoup
 
-data class NoticeListForm(val num: String, val title: String, val depart: String, val date: String,val data_id: String)
+data class NoticeListForm(val num: String, val title: String, val name: String, val date: String,val data_id: String,val notice_check: Boolean)
 
 class JsoupCrawler {
-    val WAGGLE_NOTICE_URL = "https://www.changwon.ac.kr/portal/na/ntt/selectMainAtNttList.do?mi=14003"
-    val LOGIN_PAGE_URL = "https://www.changwon.ac.kr/portal/main.do"
-    val LOGIN_ACTION_PAGE_URL = "https://www.changwon.ac.kr/portal/lo/login/ssoLogin.do"
-    val USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36"
+    val WAGGLE_NOTICE_URL = "https://www.changwon.ac.kr/portal/na/ntt/selectMainAtNttList.do?mi=14003&currPage="
+    val CE_URL = "https://www.changwon.ac.kr/ce/na/ntt/selectNttList.do?"
+    val IE_URL = "https://www.changwon.ac.kr/it/na/ntt/selectNttList.do?"
+    //val LOGIN_PAGE_URL = "https://www.changwon.ac.kr/portal/main.do"
+    //val LOGIN_ACTION_PAGE_URL = "https://www.changwon.ac.kr/portal/lo/login/ssoLogin.do"
+    //val USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36"
 
-    val waggleDataList = mutableListOf<NoticeListForm>()
-    fun waggleNotice() {
+    fun boardCrawling(where: Int) : MutableList<NoticeListForm> {
+        return boardCrawling("1", where)
+    }
+    fun boardCrawling(currPage: String, where: Int) : MutableList<NoticeListForm> {
+        val data_list = mutableListOf<NoticeListForm>()
         try {
-            val waggle_page = Jsoup.connect(WAGGLE_NOTICE_URL)
+            var url = ""
+            when(where) {
+                1 -> url = CE_URL + "mi=6627&bbsId=2187&currPage=" + currPage // 컴공 공지
+                2 -> url = CE_URL + "mi=6634&bbsId=2190&currPage=" + currPage // 컴공 자유 게시판
+                3 -> url = IE_URL + "mi=6661&bbsId=2198&currPage=" + currPage // 정통 공지
+                4 -> url = IE_URL + "mi=6665&bbsId=2199&currPage=" + currPage // 정통 수업 게시판
+                5 -> url = WAGGLE_NOTICE_URL + currPage
+                else -> throw Exception()
+            }
+            val page = Jsoup.connect(url)
                 .method(Connection.Method.GET)
                 .execute().parse()
-            val el_waggle_notice = waggle_page.getElementById("sub_content")
-            val page_list = el_waggle_notice?.select("div[class=BD_list]")?.select("table")?.select("tbody")?.select("tr")
+            val el_board = page.getElementById("sub_content")
+            val page_list = el_board
+                ?.select("div[class=BD_list]")
+                ?.select("table")
+                ?.select("tbody")
+                ?.select("tr")
+
             val list_iterator = page_list?.listIterator()
 
             while (list_iterator!!.hasNext()) {
                 val temp = list_iterator.next()
-                val num = temp.select("td[class=BD_tm_none]").first()!!.text()
+                val num = temp.select("td").first()!!.text()
+                val notice_check = num == "공지"
+
+                if(notice_check && currPage.toInt() > 1) continue
+
                 val title = temp.select("td").eachText().get(1)
-                val depart = temp.select("td").eachText().get(2)
+                val name = temp.select("td").eachText().get(2)
                 val date = temp.select("td").eachText().get(3)
                 val data_id = temp.select("a[class=nttInfoBtn]").attr("data-id")
-                println(num+": "+title+"\n"+depart+","+date+","+data_id)
-                waggleDataList.add(NoticeListForm(num,title,depart,date,data_id))
-            }
-            //println(page_list?.text())
+                println(num+": "+title+"\n"+name+","+date+","+data_id)
 
-            val el_waggle = waggle_page.select("strong[class=pc_blue]").first()
-            //val num = el_waggle?.text()?.toInt()//data-id , nttInfoBtn, post currPage 로 작동
-            //println(num)
-        }
-        catch(e: Exception) {
+                data_list.add(NoticeListForm(num,title,name,date,data_id,notice_check))
+            }
+
+        }catch (e: Exception) {
             e.printStackTrace()
         }
+        return data_list
     }
+    fun postCrawling() {
+
+    }
+/*
     fun login(id: String, pw: String) {
         try {
             val loginpage_response = Jsoup.connect(LOGIN_PAGE_URL)
@@ -78,9 +101,8 @@ class JsoupCrawler {
         catch(e: Exception) {
             e.printStackTrace()
         }
-    }
+    }*/
 }
 fun main() {
-  //  JsoupCrawler().login("","")
-    JsoupCrawler().waggleNotice()
+    JsoupCrawler().boardCrawling(5)
 }
